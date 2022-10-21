974f5aee603ff95a76335ef5d9ca8759743f5f34
januszgradzik <ja.gradzik@gmail.com>
2022-10-21 12:34:31 +0200

    Testing BE / FE

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,29 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from "axios";
 
 function App() {
+
+  const [helloMessage, setHelloMessage] = useState("")
+
+  useEffect(()=> {
+    fetchHelloMessage()
+  },[])
+
+  function fetchHelloMessage(){
+    axios.get("/api/hello")
+        .then(response => response.data)
+        .then(data => setHelloMessage(data))
+        .catch((error) => console.log(error))
+  }
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          {helloMessage}
         </p>
         <a
           className="App-link"
