9d102f05f0c1a788a31ae3c729a9fd2f89cb2255
januszgradzik <ja.gradzik@gmail.com>
2022-10-25 22:56:17 +0200

    BE - defined model

---
+++ backend/src/main/java/de/neuefische/backend/model/Tool.java
@@ -0,0 +1,29 @@
+package de.neuefische.backend.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Builder
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Document("tools")
+public class Tool {
+
+    @Id
+    private String  id;
+    private String  description;
+    private String  link;
+    private String  video;
+    private String  quizQuestion;
+    private String  choiceA;
+    private String  choiceB;
+    private String  choiceC;
+    private String  trueChoice;
+    private String  falseChoice;
+    private String  favorite;
+}
