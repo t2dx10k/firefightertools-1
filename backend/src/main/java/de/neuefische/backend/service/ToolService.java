e5d1ac92ad8509047f8e1d7bc7d265b716d1aa16
januszgradzik <ja.gradzik@gmail.com>
2022-10-29 18:33:18 +0200

    BE - FE Fix Imports

--- backend/src/main/java/de/neuefische/backend/service/ToolService.java
+++ backend/src/main/java/de/neuefische/backend/service/ToolService.java
@@ -4,7 +4,6 @@ import de.neuefische.backend.model.Tool;
 import de.neuefische.backend.repository.FireFighterToolsRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
 import java.util.List;
 
 @Service
